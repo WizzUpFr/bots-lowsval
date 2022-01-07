ac9ff3870cd6a6075f80f423819312148a5e22c7
WizzUpFr <97252016+WizzUpFr@users.noreply.github.com>
2022-01-07 13:56:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import discord
+from discord.ext import commands
 
+bot = commands.Bot(command_prefix = "?", description = "Bot de Lowsval")
+
+@bot.event
+async def on_ready():
+    print("Le bot est prêt!")
+
+
+@bot.command()
+async def clear(ctx, nombre : int):
+    messages = await ctx.channel.history(limit = nombre + 1).flatten()
+    for message in messages:
+        await message.delete()
